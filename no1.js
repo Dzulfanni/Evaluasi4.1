3688fe239ee56c06620f7a9d512493c7be48ae8b
Dzulfanni <belajarayok866@gmail.com>
2020-09-18 22:06:11 +0700

    first commit

---
+++ no1.js
@@ -0,0 +1,14 @@
+// const [ Lampung, Prabumulih, Medan , Ceribon, ...Lain ] = santriIkhwan;
+// bentuk awal
+
+var santriIkhwan = [nama= 'Sidik', asal= 'lampung', nama2= 'Rahmad', asal2= 'Medan',
+    nama3= 'Syofyan', asal3= 'Prabumulih', nama4= 'Aldi', asal4= 'Ceribon', nama5= 'Kardi', asal5= '...rest'
+] 
+{
+    document.write(nama+'<br>');
+    document.write(nama2)
+
+}
+console.log(nama3);
+console.log(nama4);
+console.log(nama5);
